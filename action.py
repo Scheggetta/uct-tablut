615c3d44bc4629b39071241370782f10a733be11
Scheggetta <edoardo.fusa@gmail.com>
2023-04-14 09:20:40 +0200

    Minor visual improvements

--- action.py
+++ action.py
@@ -1,3 +1,7 @@
+import string
+
+from_int_to_str = {i: v for i, v in zip(range(1, 10), string.ascii_uppercase[:10])}
+
 class Action:
     def __init__(self, frm: tuple, to: tuple):
         self.__from = frm
@@ -12,4 +16,8 @@ class Action:
         return self.__to
 
     def __str__(self):
-        return 'from: %s to: %s' % (self.frm, self.to)
+        frm_letter = from_int_to_str[self.frm[0]]
+        to_letter = from_int_to_str[self.to[0]]
+
+        res = 'from: (%s, %d) -> to: (%s, %d)' % (frm_letter, self.frm[1], to_letter, self.to[1])
+        return res

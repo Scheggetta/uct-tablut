dd640e489c233e3be3b9fe60631bf54718e1e839
Scheggetta <edoardo.fusa@gmail.com>
2023-04-14 09:25:43 +0200

    Revert "Minor visual improvements"
    This reverts commit ad439001810e0dd4e9fb88cb161194b6ea6a9642.

--- action.py
+++ action.py
@@ -1,8 +1,3 @@
-import string
-
-from_int_to_str = {i: v for i, v in zip(range(1, 10), string.ascii_uppercase[:10])}
-
-
 class Action:
     def __init__(self, frm: tuple, to: tuple):
         self.__from = frm
@@ -17,8 +12,4 @@ class Action:
         return self.__to
 
     def __str__(self):
-        frm_letter = from_int_to_str[self.frm[0]]
-        to_letter = from_int_to_str[self.to[0]]
-
-        res = 'from: (%s, %d) -> to: (%s, %d)' % (frm_letter, self.frm[1], to_letter, self.to[1])
-        return res
+        return 'from: %s to: %s' % (self.frm, self.to)
